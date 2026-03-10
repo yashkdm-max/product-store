fed5bea331fb710f7d812ae3d704e742b1d790aa
yashkdm-max <chmcs2026.yash.kadam@gmail.com>
2026-03-10 09:16:37 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000"
+//  baseURL: "http://localhost:3000"
+  baseURL:"https://product-store-315o.onrender.com"
 });
 
 export default api;
