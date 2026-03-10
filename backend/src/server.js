ad6b96b54f8027204337c3481edeca5348c6da32
yashkdm-max <chmcs2026.yash.kadam@gmail.com>
2026-03-10 09:06:22 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,7 +11,7 @@ const port=process.env.PORT || 3000
 dns.setServers(["1.1.1.1","8.8.8.8"])
 app.use(cors(
     {
-        origin:'http://localhost:5173'
+        //origin:'http://localhost:5173'
     }
 ))
 // app.get('/',(req,res)=>{
